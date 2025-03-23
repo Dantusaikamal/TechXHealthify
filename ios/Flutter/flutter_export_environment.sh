500d313a473c3c64c51b3d3aff46b2bc3db17ce3
dantusaikamal <dantusaikamal@gmail.com>
2025-03-23 14:17:00 +0530

    Android and ios fixes

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Android\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\dantu\OneDrive\Desktop\BITS Techxcelerate\TechxHealthify"
+export "FLUTTER_APPLICATION_PATH=C:\Users\dantu\OneDrive\Desktop\BITS Techxcelerate\TechXHealthify"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
